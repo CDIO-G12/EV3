cb4d85dcfe1be4b3933127c389998ed2ac5432e2
ViTeXFTW <90150876+ViTeXFTW@users.noreply.github.com>
2023-03-22 10:11:50 +0100

    Add files via upload

---
+++ MovementController.java
@@ -0,0 +1,145 @@
+
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.robotics.RegulatedMotor;
+
+public class MovementController extends Thread{
+
+	private float wheelSize;
+	private float wheelDistance;
+	private RegulatedMotor left;
+	private RegulatedMotor right;
+	
+	private float wheelRadius;
+	private float wheelDistanceRadius;
+	private float wheelCircumference;
+	private float robotCircumference;
+	
+	private float turnConversion;
+	private float distancePrDegree;
+	
+	public MovementController(Port left, Port right, float wheelSize, float wheelDistance) {
+		
+		this.wheelSize = wheelSize;
+		this.wheelDistance = wheelDistance;
+		
+		this.left = new EV3LargeRegulatedMotor(left);
+		this.right = new EV3LargeRegulatedMotor(right);
+		
+		this.left.synchronizeWith(new RegulatedMotor[] {this.right});
+		
+		setupVariables();
+		
+	}
+	
+	public MovementController(float wheelSize, float wheelDistance, RegulatedMotor left, RegulatedMotor right,
+			RegulatedMotor harvester) {
+		
+		this.wheelSize = wheelSize;
+		this.wheelDistance = wheelDistance;
+		this.left = left;
+		this.right = right;
+		
+		setupVariables();
+		
+	}
+	
+	private void setupVariables() {
+
+		// Variables for the small wheel
+		wheelRadius = wheelSize/2;
+		wheelCircumference = (float) (wheelRadius * Math.PI * 2);
+		
+		// Variables for the robot as a whole
+		wheelDistanceRadius = wheelDistance / 2;
+		robotCircumference = (float) (wheelDistanceRadius * Math.PI * 2);
+		
+		// How far the robot turns when asked to turn 1 degree
+		distancePrDegree = wheelCircumference/360;
+		
+
+		// TODO: Currently set, needs to be changed to a calculation. 
+		// Means 6.444 turns the robot 1 degree
+		// turnConversion = 6.444f;
+		turnConversion = robotCircumference / wheelCircumference;
+		
+	}
+	
+	
+	/*
+	 * Takes the input argument (in millimeters) and moves that distance.
+	 */
+	public void moveForward(int Distance) {
+		
+		int degreesToTurn = (int) (distancePrDegree / Distance);
+		
+		left.startSynchronization();
+		left.rotate(degreesToTurn);
+		right.rotate(degreesToTurn);
+		left.endSynchronization();
+		
+	}
+	
+	
+	public void moveBackward(int Distance) {
+		
+		int degreesToTurn = (int) -(distancePrDegree / Distance);
+		
+		left.startSynchronization();
+		left.rotate(degreesToTurn);
+		right.rotate(degreesToTurn);
+		left.endSynchronization();
+		
+	}
+	
+	public void turnRight(int degrees) {
+		
+		int totalDegrees = (int) (degrees * turnConversion);
+		
+		left.startSynchronization();
+		left.rotate(-totalDegrees);
+		right.rotate(totalDegrees);
+		left.endSynchronization();
+		
+	}
+	
+	public void turnLeft(int degrees) {
+		
+		int totalDegrees = (int) (degrees * turnConversion);
+		
+		left.startSynchronization();
+		left.rotate(totalDegrees);
+		right.rotate(-totalDegrees);
+		left.endSynchronization();
+		
+	}
+	
+	public void stopMovement() {
+		
+		left.startSynchronization();
+		left.rotate(0);
+		right.rotate(0);
+		left.endSynchronization();
+		
+	}
+	
+	public boolean isMoving() {
+		
+		return (left.isMoving() || right.isMoving());
+		
+	}
+	
+	public boolean isStalled() {
+		
+		return (left.isStalled() || right.isStalled());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+
+public class main {
+
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		
+		Robot robot = new Robot();
+		robot.run();
+		
+		
+
+	}
+
+}
