a8be886ff9e4a1891d5695785a27b650a9a34b65
Mads Rasmussen <s215521@dtu.dk>
2023-06-07 14:29:53 +0200

    Before added new thread for pickup

--- src/main.java
+++ src/main.java
@@ -1,13 +1,6 @@
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-import lejos.hardware.ev3.EV3;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.Port;
-import lejos.hardware.port.SensorPort;
-import lejos.internal.ev3.EV3Port;
-import lejos.utility.Delay;
-
 public class main {
 
 	
